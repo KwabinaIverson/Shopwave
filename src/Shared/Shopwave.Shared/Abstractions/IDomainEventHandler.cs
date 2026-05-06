b87d90642b7040d876f82a1f26ba9416416ff7b9
Benjamin Atsu <Bredrenbonish@gmail.com>
2026-05-06 23:30:24 +0000

    Revert "Merge pull request #5 from KwabinaIverson/feature/identity-api"
    This reverts commit 2e0d32896030aa9784ff2984689dacc0ab470a81, reversing
    changes made to 6697fdffec1b54f6fe4eb2d402eff2b2852c9478.

--- src/Shared/Shopwave.Shared/Abstractions/IDomainEventHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace Shopwave.Shared.Abstractions;
-
-public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
-{
-    Task Handle(TEvent domainEvent, CancellationToken cancellationToken = default);
-}
